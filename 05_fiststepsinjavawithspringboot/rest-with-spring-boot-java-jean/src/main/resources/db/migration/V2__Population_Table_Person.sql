f885aec872c998b5aac358138de03218d92f1d2f
jfabb <fabbio.2k@gmail.com>
2023-12-04 19:36:41 -0300

    Putting Migrations in my Software

---
+++ 05_fiststepsinjavawithspringboot/rest-with-spring-boot-java-jean/src/main/resources/db/migration/V2__Population_Table_Person.sql
@@ -0,0 +1,7 @@
+
+
+
+INSERT INTO `person` VALUES (1,'Rua das flores, 123, Ipiranga, Garapu, Cabo,PE','Jordan Mcdilan','male','Silva'),(2,'Rua São Paulo, Bairro dos Estados,João Pessoa, PB','Andreza de Sá','female','Gomes'),(3,'Rua São Paulo, Bairro dos Estados,João Pessoa, PB','Jean Fabio','male','Lopes'),(4,'Rua princesa Geneveva, 152, Alto do Mandu, São Cristovan, Rio de Janeiro,PE','Ricardo Silva ','male','Costa');
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
+
